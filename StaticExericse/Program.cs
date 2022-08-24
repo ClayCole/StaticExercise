c0b64dc772d07d98f35bd92ec9eb44be22453022
Clay Coleman <claycoleman94@gamil.com>
2022-08-23 23:31:10 -0400

    inintal Upload

---
+++ StaticExericse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StaticExericse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var Celsius = TempConverter.FahrenheitToCelsius(75);
+            Console.WriteLine($"its currently {Celsius} degrees in Celsius");
+
+            var Fahrenheit = TempConverter.CelsiusToFahrenheit(34);
+            Console.WriteLine($"its currently {Fahrenheit} degrees in Fahrenheit");
+        }
+     
+        
+    }
+}
+
---
+++ StaticExericse/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExericse
+{
+    public static class TempConverter
+    {
+
+        public static double FahrenheitToCelsius(double fah)
+        {
+            var resultsF = (fah - 32) / 1.8;
+            return Math.Round(resultsF,2);
+        }
+
+        public static double CelsiusToFahrenheit(double cel)
+        {
+            var resultsC = (cel * 1.8) + 32;
+            return Math.Round(resultsC,2);
+        }
+    }
+
+}
+
